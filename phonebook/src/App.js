bdcc378683e8a70e0cebe44af23066e20a710be3
leeinsong <insong1216@gmail.com>
2021-05-11 14:20:09 +0900

    어렵다 git

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,14 +1,44 @@
 import React, { Component } from "react";
 import PhoneForm from "./components/PhoneForm";
-export default class App extends Component {
+import PhoneInfoList from "./components/PhoneInfoList";
+
+class App extends Component {
+  id = 2;
+  state = {
+    information: [
+      {
+        id: 0,
+        name: "김민준",
+        phone: "010-0000-0000",
+      },
+      {
+        id: 1,
+        name: "홍길동",
+        phone: "010-0000-0001",
+      },
+    ],
+  };
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data }),
+    });
+  };
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter((info) => info.id !== id),
+    });
   };
   render() {
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList data={information} onRemove={this.handleRemove} />
       </div>
     );
   }
 }
+
+export default App;
---
+++ phonebook/src/components/PhoneInfoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PhoneInfo from "./PhoneInfo";
+
+export default class PhoneInfoList extends Component {
+  static defaultProps = {
+    data: [],
+    onRemove: () => console.log("onRemove not defined"),
+  };
+
+  render() {
+    const { data, onRemove } = this.props;
+    console.log(this.props);
+    console.log("Hello World");
+    const list = data.map((info) => <PhoneInfo key={info.id} info={info} onRemove={onRemove} />);
+    return <div>{list}</div>;
+  }
+}
